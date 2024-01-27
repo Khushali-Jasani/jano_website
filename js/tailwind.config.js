a9dd101d440e6a288906c088d60090847041e56c
milansir <rwn3developer11@gmail.com>
2024-01-27 11:09:31 +0530

    first commit

---
+++ js/tailwind.config.js
@@ -0,0 +1,18 @@
+tailwind.config = {
+  content: [],
+  theme: {
+    fontFamily:{
+      'inter': ['Inter', 'sans-serif'],
+    },
+    extend: {
+      colors: {
+        'primery': '#FF3294',
+        'border': '#E9E9E9',
+        'bg-gray': '#fafafa',
+        'border2': '#dcdcdc',
+      },
+    },
+  },
+  plugins: [],
+}
+
